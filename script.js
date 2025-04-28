369083559ffe69ecc3f6166f4b48e7ed27179a69
Renilto Silva <39367292+ReniltoSilva@users.noreply.github.com>
2025-04-27 21:49:13 -0500

    fixed bug with darkMode function

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const darkModeButton = document.querySelector('#button-Dark-Mode')
 const textDarkMode = document.querySelector('.text-dark-button')
 const darkModeIcon = document.querySelector('.fa-solid')
-const savedThemeUser = JSON.parse(localStorage.getItem('Theme User')).theme;
 
 darkModeButton.addEventListener('click', darkModeSwitcher)
-document.addEventListener('DOMContentLoaded', checkThemeUser(savedThemeUser))
+document.addEventListener('DOMContentLoaded', checkThemeUser())
 
 //Light Mode Function
 function lightMode(){
@@ -45,12 +44,32 @@ function savethemeUser(theme){
 }
 
 //Check Theme in LocalSorage
-function checkThemeUser(savedThemeUser){
-    if(savedThemeUser === 'light'){
-        document.documentElement.setAttribute('data-theme', 'light')
-        lightMode()
+function checkThemeUser(){
+    const savedTheme = JSON.parse(localStorage.getItem('Theme User'));
+    console.log(savedTheme)
+    if(!savedTheme){
+        savethemeUser('Light')
     }else{
-        document.documentElement.setAttribute('data-theme', 'dark')
-        darkMode()
+        if(savedTheme.theme === 'light'){
+            document.documentElement.setAttribute('data-theme', 'light')
+            lightMode()
+        }else{
+            document.documentElement.setAttribute('data-theme', 'dark')
+            darkMode()
+        } 
     }
+}
+
+//Navigation
+const nav = document.getElementById('nav')
+const menuIcon = document.querySelector('.menu-icon')
+
+function toggleMenu() {
+  nav.classList.toggle('active');
+  menuIcon.classList.toggle('active')
+}
+
+function hideMenu(){
+  nav.classList.remove('active')
+  menuIcon.classList.remove('active')
 }
